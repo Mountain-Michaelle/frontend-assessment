e4317605b362477ea777af15235ccc1536040e75
Mountain-Michaelle <michaelchinemelu24@gmail.com>
2025-12-26 00:46:53 +0100

    tailwind config import error fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-import { BackgroundVariant } from "reactflow";
-
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -10,27 +8,18 @@ module.exports = {
   darkMode: "class",
   theme: {
     container: {
-      center: true,
-      padding: "1rem",
-    },
     extend: {
       colors: {
         danger: 'red',
         brd:'#e0e0e0',
-        grn:'#013941',
-        
-      },
-       border: {
-          subtle: "#F1F5F9",
-        },
-      
-      backgroundImage: {
-        custom_gradient : 'linear-gradient(to bottom, #FFF5F5 63%, #756969 100%)',
+        grn:'#013941',        
       },
       backgroundColor:{
          btn_bg:'var(--btn-bg)'
       }
    },
   },
+
   plugins: [],
-};
+} 
+}
